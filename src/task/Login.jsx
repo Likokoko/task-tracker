675d07324ad724d34909abc0173b79df62d187d2
Li-Ko Chien <>
2023-04-06 22:57:09 +0800

    new

--- src/task/Login.jsx
+++ src/task/Login.jsx
@@ -23,17 +23,21 @@ function Login() {
   const [loading, setLoading] = useState(false);
   console.log("my token: ", getToken());
 
+  /**
+   * browser request ->
+   * http://localhost:8080 (proxy: github)
+   * 8080 -> github
+   * github response -> 8080 (no CORS)
+   * 8080 response -> 3000
+   */
+
   useEffect(() => {
     const getToken = async (code) => {
-      /**
-       * browser request ->
-       * http://localhost:8080 (proxy: github)
-       * 8080 -> github
-       * github response -> 8080 (no CORS)
-       * 8080 response -> 3000
-       */
+        console.log(
+          `${REACT_APP_PROXY_URL}${REACT_APP_TOKEN_URL}?client_id=${REACT_APP_CLIENT_ID}&client_secret=${REACT_APP_CLIENT_SECRET}&redirect_uri=${REACT_APP_REDIRECT_URI}&code=${code}`
+        );
       return fetch(
-        `${REACT_APP_PROXY_URL}${REACT_APP_TOKEN_URL}?client_id=${REACT_APP_CLIENT_ID}&client_secret=${REACT_APP_CLIENT_SECRET}&redirect_uri=${REACT_APP_REDIRECT_URI}&code=${code}`,
+       `${REACT_APP_PROXY_URL}${REACT_APP_TOKEN_URL}?client_id=${REACT_APP_CLIENT_ID}&client_secret=${REACT_APP_CLIENT_SECRET}&redirect_uri=${REACT_APP_REDIRECT_URI}&code=${code}`,
         {
           method: "POST",
           headers: {
@@ -54,23 +58,26 @@ function Login() {
         });
     };
 
+    //get the code and send to getToken
     const parser = UrlParser(window.location.href);
     const q = parser.queryParams;
+
     if (q.code) {
       getToken(q.code);
     } else {
-      console.log("no code...");
+      console.log("no code");
     }
   }, []);
 
+  //if login clicked, request made and waiting for a response
   const handleLogin = async () => {
     setLoading(true);
     try {
-      await login();
+      await login(); // initiate the authentication with github
     } catch (err) {
       console.error(err);
     } finally {
-      setLoading(false);
+      setLoading(false); //request has completed,  re-render to show the updated
     }
   };
 
@@ -80,7 +87,7 @@ function Login() {
       data-aos="fade-up"
       data-aos-anchor-placement="top-center"
     >
-      <a
+      <a onClick={handleLogin}
         href={`${REACT_APP_AUTH_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URI}&login=Likokoko&scope=repo&allow_signup=false`}
       >
         Login with GitHub
